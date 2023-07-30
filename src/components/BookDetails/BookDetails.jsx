b49981c12e4780a7f94be4a6576f3fbb22d7b3cb
Alexandru Besleaga <alex@MacBook-Pro-Alexandru.local>
2023-07-31 01:23:14 +0300

    refactoring code

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,12 +1,14 @@
 import {useSelector} from "react-redux";
+import noBookImage from '../../assets/nobookimage.jpeg';
 
 const BookDetails =()=>{
+
     const {bookProp} = useSelector(state=>state.libraryReducer);
 
     return (
         <div style={{width:'70vw',border:'2px solid #3d8eff ',height:'40vh',marginLeft:30,marginTop:20, padding:20}}>
             <h1>{bookProp.author}</h1>
-            <img src={bookProp.thumbnail} alt={bookProp.title}/>
+            <img className={bookProp.thumbnail ? 'detailsBookImage' : 'detailsNoBookImage'} src={bookProp.thumbnail ? bookProp.thumbnail : noBookImage} alt={bookProp.title}/>
             <h2>{bookProp.url}</h2>
             <p>{bookProp.description}</p>
 
