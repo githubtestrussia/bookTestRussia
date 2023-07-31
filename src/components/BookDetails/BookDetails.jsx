c88fc46a1bef78ecf8ebf737590a6e89fdf7d802
Alexandru Besleaga <alex@MacBook-Pro-Alexandru.local>
2023-07-31 09:54:19 +0300

    last commit

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -6,7 +6,7 @@ const BookDetails =()=>{
     const {bookProp} = useSelector(state=>state.libraryReducer);
 
     return (
-        <div style={{width:'70vw',border:'2px solid #3d8eff ',height:'40vh',marginLeft:30,marginTop:20, padding:20}}>
+        <div style={{width:'70vw',border:'2px solid #3d8eff ',height:'auto',marginLeft:30,marginTop:20, padding:20}}>
             <h1>{bookProp.author}</h1>
             <img className={bookProp.thumbnail ? 'detailsBookImage' : 'detailsNoBookImage'} src={bookProp.thumbnail ? bookProp.thumbnail : noBookImage} alt={bookProp.title}/>
             <h2>{bookProp.url}</h2>
